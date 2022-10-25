3656016a12f6cc077f78b8056bbe5813f89f7c81
“Jesse <jesse.kercheval@gmail.com>
2022-10-25 20:08:23 +0000

    new codewars

--- train_5kyu/order_weight.rb
+++ train_5kyu/order_weight.rb
@@ -25,8 +25,8 @@ output:
 # end
 
 def order_weight(str)
-  str.split.sort_by { |st| [st.chars.map {|s| s.to_i}.sum, st] }.join(' ')
+  str.split.sort_by { |st| [st.to_i.digits.sum, st] }.join(' ')
 end
 
-p order_weight("103 123 4444 99 2000") #== "2000 103 123 4444 99"
-p order_weight("2000 10003 1234000 44444444 9999 11 11 22 123") #== "11 11 2000 10003 22 123 1234000 44444444 9999"
+p order_weight("103 123 4444 99 2000") == "2000 103 123 4444 99"
+p order_weight("2000 10003 1234000 44444444 9999 11 11 22 123") == "11 11 2000 10003 22 123 1234000 44444444 9999"
